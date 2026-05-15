422c5efe0f999bec7963c85299f71b2486b66445
Ren <abhijaisharma1@gmail.com>
2026-05-16 02:48:26 +0530

    debugging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from src.api.ingest import router as ingest_router
+from src.api.query import router as query_router
+from src.api.reset import router as reset_router
+from src.api.document import router as doc_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def root():
-
-    return {"message": "working"}
+app.include_router(ingest_router, prefix="/api", tags=["ingestion"])
+app.include_router(query_router, prefix="/api", tags=["query"])
+app.include_router(reset_router, prefix="/api", tags=["reset"])
+app.include_router(doc_router, prefix="/api", tags=["documents"])
