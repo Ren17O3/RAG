2ee0f2b86b7127bbe18b5c507b0ffbe35142a267
Ren <abhijaisharma1@gmail.com>
2026-05-16 00:47:03 +0530

    changed models

--- src/api/query.py
+++ src/api/query.py
@@ -10,11 +10,10 @@ from src.services.query_rewriter import rewrite_query
 
 router = APIRouter()
 
-retriever = Retriever()
-
 
 @router.post("/query")
 async def query_documents(request: QueryRequest):
+    retriever = Retriever()
 
     original_query = request.query
 
