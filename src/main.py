40118294eee5361c2d555a6f26a13194f6aa4993
Ren <abhijaisharma1@gmail.com>
2026-05-16 02:40:26 +0530

    debugging

--- src/main.py
+++ src/main.py
@@ -1,25 +1,4 @@
 from fastapi import FastAPI
-from src.api.ingest import router as ingest_router
-from src.api.query import router as query_router
-from src.api.reset import router as reset_router
-from src.api.document import router as doc_router
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.include_router(ingest_router, prefix="/api", tags=["ingestion"])
-app.include_router(query_router, prefix="/api", tags=["query"])
-app.include_router(reset_router, prefix="/api", tags=["reset"])
-app.include_router(doc_router, prefix="/api", tags=["documents"])
-
-from fastapi import FastAPI
 
 app = FastAPI()
 
