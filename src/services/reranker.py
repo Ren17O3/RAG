ce2ea16a1e07d062944afe13407749bc07455ed9
Ren <abhijaisharma1@gmail.com>
2026-05-16 02:41:00 +0530

    debugging

--- src/services/reranker.py
+++ src/services/reranker.py
@@ -3,9 +3,16 @@ from sentence_transformers import CrossEncoder
 
 class Reranker:
 
-    def __init__(self):
+    _model = None
 
-        self.model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    @classmethod
+    def get_model(cls):
+
+        if cls._model is None:
+
+            cls._model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+        return cls._model
 
     def rerank(self, query: str, results: list):
 
